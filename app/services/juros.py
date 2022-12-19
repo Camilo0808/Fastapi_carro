7105ff600070e3dbdbf3297b31d8ab4fcfa75c14
Camilo0808 <lorenzo.camilo92@gmail.com>
2022-12-19 09:28:06 -0500

    Editando classe Financiamento

--- app/services/juros.py
+++ app/services/juros.py
@@ -2,18 +2,23 @@
 
 class Financiamento():
 
-    def __init__(self, preco, parcelas, juros, preco_juros=0):
-        
-        self. preco = preco
-        self.parcelas = parcelas
-        self.juros = juros
-        preco_juros = preco+(preco*(juros/parcelas)*parcelas)
-        juros_mensal = (juros/parcelas)*100     
+    def __init__(self, juros=0.3):
+        self.juros = juros    
 
+
+    def calculo_juros(self, preco, parcelas):
+        self.preco = preco
+        self.parcelas = parcelas
+        preco_juros = preco+(preco*(self.juros/parcelas)*parcelas)
+        juros_mensal = (self.juros/parcelas)*100     
+        self.mostra_valores(preco_juros, juros_mensal)   
+
+    def mostra_valores(self, preco_juros, juros_mensal):
         print(f'O valor total do carro é de R$:',format(preco_juros, '.2f'))
         print('O juros mensal será de:', format(juros_mensal,'.2f'),'%')
 
-financiamento_marcelo = Financiamento(50000, 24, 0.20)
 
+financiamento_marcelo = Financiamento()
+financiamento_marcelo.calculo_juros(50000, 24)
 
 
